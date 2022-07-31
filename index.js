e7cd08e1f11fdaaa480e4b792409710250fc2c06
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2022-07-31 13:48:12 -0300

    Adicionada descrição radio de galena

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ const projectDescription = document.getElementById("project-description");
 const projectImage = document.getElementById("project-image");
 const projectName = document.getElementById("project-name");
 
+
+const descriptionRadioDeGalena = 
+"Este projeto foi desenvolvido para a materia de Fisica 3, o objetivo é realizar a simulação da operação de um rádio de galena. Este programa realiza a simulação dês da criação do sinal por uma transmissora AM, até a captação, retificação de demodulação do sinal.<br><br>Para a realização deste trabalho foi utilizado a engine de jogos Godot, mais informações podem ser encontradas no repositório do projeto.";
+
+
 function updateContents()
 {
     switch(currentProject)
     {
         case "radio-de-galena":
-            projectDescription.innerHTML="aaa"; 
+            projectDescription.innerHTML= descriptionRadioDeGalena; 
             projectImage.src = "images/Projeto1img.png";
             projectName.innerHTML="Rádio de Galena";
         break;
@@ -82,3 +87,7 @@ document.getElementById("repository-btn").onclick = function () {
         break;  
     }
 };
+
+
+
+updateContents();
